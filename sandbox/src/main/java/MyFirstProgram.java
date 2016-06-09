3c59b200b3d7b2bfbcc1bfcaf8e79ac02f557c78
spodgorov <podgorov@gmail.com>
2016-06-10 02:32:56 +0300

    Добавил новый код

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,5 +1,8 @@
 public class MyFirstProgram {
   public static void main (String [] args) {
-	System.out.println("Hello, Guys");
+	  int a=2;
+		int b=3;
+	  int c=a*b;
+	  System.out.println(c);
 	}
 }
